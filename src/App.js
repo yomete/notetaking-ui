91fe531a63893cec22dabbbb803f2bc2453edd38
yomete <iamlese@yahoo.com>
2019-09-29 21:15:13 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import AllNotes from "./AllNotes";
+import NewNote from "./NewNote";
+import EditNote from "./EditNote";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Router>
+      <div>
+        <nav
+          className="navbar App-header"
+          role="navigation"
+          aria-label="main navigation"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="navbar-brand">
+            <Link to="/" className="navbar-item">
+              NotesQL
+            </Link>
+          </div>
+
+          <div className="navbar-end">
+            <Link to="/" className="navbar-item">
+              All Notes
+            </Link>
+
+            <Link to="/newnote" className="navbar-item">
+              New Note
+            </Link>
+          </div>
+        </nav>
+
+        <Route exact path="/" component={AllNotes} />
+        <Route path="/newnote" component={NewNote} />
+        <Route path="/note/:id" component={EditNote} />
+      </div>
+    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from "@apollo/react-hooks";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { ApolloLink } from "apollo-link";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+import { onError } from "apollo-link-error";
+import Notifications, { notify } from "react-notify-toast";
+
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const errorLink = onError(({ graphQLErrors }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message }) => notify.show(message, "error"));
+});
+
+const httpLink = createHttpLink({ uri: "http://localhost:4300/graphql" });
+
+const link = ApolloLink.from([errorLink, httpLink]);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Notifications />
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
